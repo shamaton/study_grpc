51b7bdf5912f7505df2fdd4827f6c16096f2edd2
shamaton <shamaton77@gmail.com>
2018-08-07 18:48:42 +0900

    add event exit

--- chat/proto/chat.pb.go
+++ chat/proto/chat.pb.go
@@ -33,7 +33,7 @@ func (m *None) Reset()         { *m = None{} }
 func (m *None) String() string { return proto.CompactTextString(m) }
 func (*None) ProtoMessage()    {}
 func (*None) Descriptor() ([]byte, []int) {
-	return fileDescriptor_chat_fe9674db1763fdcb, []int{0}
+	return fileDescriptor_chat_3f2ec72249544206, []int{0}
 }
 func (m *None) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_None.Unmarshal(m, b)
@@ -64,7 +64,7 @@ func (m *RequestAuthorize) Reset()         { *m = RequestAuthorize{} }
 func (m *RequestAuthorize) String() string { return proto.CompactTextString(m) }
 func (*RequestAuthorize) ProtoMessage()    {}
 func (*RequestAuthorize) Descriptor() ([]byte, []int) {
-	return fileDescriptor_chat_fe9674db1763fdcb, []int{1}
+	return fileDescriptor_chat_3f2ec72249544206, []int{1}
 }
 func (m *RequestAuthorize) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_RequestAuthorize.Unmarshal(m, b)
@@ -102,7 +102,7 @@ func (m *ResponseAuthorize) Reset()         { *m = ResponseAuthorize{} }
 func (m *ResponseAuthorize) String() string { return proto.CompactTextString(m) }
 func (*ResponseAuthorize) ProtoMessage()    {}
 func (*ResponseAuthorize) Descriptor() ([]byte, []int) {
-	return fileDescriptor_chat_fe9674db1763fdcb, []int{2}
+	return fileDescriptor_chat_3f2ec72249544206, []int{2}
 }
 func (m *ResponseAuthorize) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_ResponseAuthorize.Unmarshal(m, b)
@@ -140,7 +140,7 @@ func (m *RequestConnect) Reset()         { *m = RequestConnect{} }
 func (m *RequestConnect) String() string { return proto.CompactTextString(m) }
 func (*RequestConnect) ProtoMessage()    {}
 func (*RequestConnect) Descriptor() ([]byte, []int) {
-	return fileDescriptor_chat_fe9674db1763fdcb, []int{3}
+	return fileDescriptor_chat_3f2ec72249544206, []int{3}
 }
 func (m *RequestConnect) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_RequestConnect.Unmarshal(m, b)
@@ -179,7 +179,7 @@ func (m *CommandSay) Reset()         { *m = CommandSay{} }
 func (m *CommandSay) String() string { return proto.CompactTextString(m) }
 func (*CommandSay) ProtoMessage()    {}
 func (*CommandSay) Descriptor() ([]byte, []int) {
-	return fileDescriptor_chat_fe9674db1763fdcb, []int{4}
+	return fileDescriptor_chat_3f2ec72249544206, []int{4}
 }
 func (m *CommandSay) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_CommandSay.Unmarshal(m, b)
@@ -213,12 +213,51 @@ func (m *CommandSay) GetMessage() string {
 	return ""
 }
 
+type CommandLeave struct {
+	SessionId            []byte   `protobuf:"bytes,1,opt,name=session_id,json=sessionId,proto3" json:"session_id,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *CommandLeave) Reset()         { *m = CommandLeave{} }
+func (m *CommandLeave) String() string { return proto.CompactTextString(m) }
+func (*CommandLeave) ProtoMessage()    {}
+func (*CommandLeave) Descriptor() ([]byte, []int) {
+	return fileDescriptor_chat_3f2ec72249544206, []int{5}
+}
+func (m *CommandLeave) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_CommandLeave.Unmarshal(m, b)
+}
+func (m *CommandLeave) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_CommandLeave.Marshal(b, m, deterministic)
+}
+func (dst *CommandLeave) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_CommandLeave.Merge(dst, src)
+}
+func (m *CommandLeave) XXX_Size() int {
+	return xxx_messageInfo_CommandLeave.Size(m)
+}
+func (m *CommandLeave) XXX_DiscardUnknown() {
+	xxx_messageInfo_CommandLeave.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_CommandLeave proto.InternalMessageInfo
+
+func (m *CommandLeave) GetSessionId() []byte {
+	if m != nil {
+		return m.SessionId
+	}
+	return nil
+}
+
 type Event struct {
 	// Types that are valid to be assigned to Event:
 	//	*Event_None
 	//	*Event_Join
 	//	*Event_Leave
 	//	*Event_Log
+	//	*Event_Exit
 	Event                isEvent_Event `protobuf_oneof:"event"`
 	XXX_NoUnkeyedLiteral struct{}      `json:"-"`
 	XXX_unrecognized     []byte        `json:"-"`
@@ -229,7 +268,7 @@ func (m *Event) Reset()         { *m = Event{} }
 func (m *Event) String() string { return proto.CompactTextString(m) }
 func (*Event) ProtoMessage()    {}
 func (*Event) Descriptor() ([]byte, []int) {
-	return fileDescriptor_chat_fe9674db1763fdcb, []int{5}
+	return fileDescriptor_chat_3f2ec72249544206, []int{6}
 }
 func (m *Event) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_Event.Unmarshal(m, b)
@@ -269,6 +308,10 @@ type Event_Log struct {
 	Log *EventLog `protobuf:"bytes,4,opt,name=log,proto3,oneof"`
 }
 
+type Event_Exit struct {
+	Exit *EventExit `protobuf:"bytes,5,opt,name=exit,proto3,oneof"`
+}
+
 func (*Event_None) isEvent_Event() {}
 
 func (*Event_Join) isEvent_Event() {}
@@ -276,6 +319,8 @@ func (*Event_Join) isEvent_Event() {}
 func (*Event_Leave) isEvent_Event() {}
 
 func (*Event_Log) isEvent_Event() {}
+
+func (*Event_Exit) isEvent_Event() {}
 
 func (m *Event) GetEvent() isEvent_Event {
 	if m != nil {
@@ -312,6 +357,13 @@ func (m *Event) GetLog() *EventLog {
 	return nil
 }
 
+func (m *Event) GetExit() *EventExit {
+	if x, ok := m.GetEvent().(*Event_Exit); ok {
+		return x.Exit
+	}
+	return nil
+}
+
 // XXX_OneofFuncs is for the internal use of the proto package.
 func (*Event) XXX_OneofFuncs() (func(msg proto.Message, b *proto.Buffer) error, func(msg proto.Message, tag, wire int, b *proto.Buffer) (bool, error), func(msg proto.Message) (n int), []interface{}) {
 	return _Event_OneofMarshaler, _Event_OneofUnmarshaler, _Event_OneofSizer, []interface{}{
@@ -319,6 +371,7 @@ func (*Event) XXX_OneofFuncs() (func(msg proto.Message, b *proto.Buffer) error, 
 		(*Event_Join)(nil),
 		(*Event_Leave)(nil),
 		(*Event_Log)(nil),
+		(*Event_Exit)(nil),
 	}
 }
 
@@ -344,6 +397,11 @@ func _Event_OneofMarshaler(msg proto.Message, b *proto.Buffer) error {
 	case *Event_Log:
 		b.EncodeVarint(4<<3 | proto.WireBytes)
 		if err := b.EncodeMessage(x.Log); err != nil {
+			return err
+		}
+	case *Event_Exit:
+		b.EncodeVarint(5<<3 | proto.WireBytes)
+		if err := b.EncodeMessage(x.Exit); err != nil {
 			return err
 		}
 	case nil:
@@ -388,6 +446,14 @@ func _Event_OneofUnmarshaler(msg proto.Message, tag, wire int, b *proto.Buffer) 
 		err := b.DecodeMessage(msg)
 		m.Event = &Event_Log{msg}
 		return true, err
+	case 5: // event.exit
+		if wire != proto.WireBytes {
+			return true, proto.ErrInternalBadWireType
+		}
+		msg := new(EventExit)
+		err := b.DecodeMessage(msg)
+		m.Event = &Event_Exit{msg}
+		return true, err
 	default:
 		return false, nil
 	}
@@ -417,6 +483,11 @@ func _Event_OneofSizer(msg proto.Message) (n int) {
 		n += 1 // tag and wire
 		n += proto.SizeVarint(uint64(s))
 		n += s
+	case *Event_Exit:
+		s := proto.Size(x.Exit)
+		n += 1 // tag and wire
+		n += proto.SizeVarint(uint64(s))
+		n += s
 	case nil:
 	default:
 		panic(fmt.Sprintf("proto: unexpected type %T in oneof", x))
@@ -434,7 +505,7 @@ func (m *EventNone) Reset()         { *m = EventNone{} }
 func (m *EventNone) String() string { return proto.CompactTextString(m) }
 func (*EventNone) ProtoMessage()    {}
 func (*EventNone) Descriptor() ([]byte, []int) {
-	return fileDescriptor_chat_fe9674db1763fdcb, []int{6}
+	return fileDescriptor_chat_3f2ec72249544206, []int{7}
 }
 func (m *EventNone) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_EventNone.Unmarshal(m, b)
@@ -465,7 +536,7 @@ func (m *EventJoin) Reset()         { *m = EventJoin{} }
 func (m *EventJoin) String() string { return proto.CompactTextString(m) }
 func (*EventJoin) ProtoMessage()    {}
 func (*EventJoin) Descriptor() ([]byte, []int) {
-	return fileDescriptor_chat_fe9674db1763fdcb, []int{7}
+	return fileDescriptor_chat_3f2ec72249544206, []int{8}
 }
 func (m *EventJoin) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_EventJoin.Unmarshal(m, b)
@@ -503,7 +574,7 @@ func (m *EventLeave) Reset()         { *m = EventLeave{} }
 func (m *EventLeave) String() string { return proto.CompactTextString(m) }
 func (*EventLeave) ProtoMessage()    {}
 func (*EventLeave) Descriptor() ([]byte, []int) {
-	return fileDescriptor_chat_fe9674db1763fdcb, []int{8}
+	return fileDescriptor_chat_3f2ec72249544206, []int{9}
 }
 func (m *EventLeave) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_EventLeave.Unmarshal(m, b)
@@ -542,7 +613,7 @@ func (m *EventLog) Reset()         { *m = EventLog{} }
 func (m *EventLog) String() string { return proto.CompactTextString(m) }
 func (*EventLog) ProtoMessage()    {}
 func (*EventLog) Descriptor() ([]byte, []int) {
-	return fileDescriptor_chat_fe9674db1763fdcb, []int{9}
+	return fileDescriptor_chat_3f2ec72249544206, []int{10}
 }
 func (m *EventLog) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_EventLog.Unmarshal(m, b)
@@ -576,17 +647,49 @@ func (m *EventLog) GetMessage() string {
 	return ""
 }
 
+type EventExit struct {
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *EventExit) Reset()         { *m = EventExit{} }
+func (m *EventExit) String() string { return proto.CompactTextString(m) }
+func (*EventExit) ProtoMessage()    {}
+func (*EventExit) Descriptor() ([]byte, []int) {
+	return fileDescriptor_chat_3f2ec72249544206, []int{11}
+}
+func (m *EventExit) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_EventExit.Unmarshal(m, b)
+}
+func (m *EventExit) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_EventExit.Marshal(b, m, deterministic)
+}
+func (dst *EventExit) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_EventExit.Merge(dst, src)
+}
+func (m *EventExit) XXX_Size() int {
+	return xxx_messageInfo_EventExit.Size(m)
+}
+func (m *EventExit) XXX_DiscardUnknown() {
+	xxx_messageInfo_EventExit.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_EventExit proto.InternalMessageInfo
+
 func init() {
 	proto.RegisterType((*None)(nil), "None")
 	proto.RegisterType((*RequestAuthorize)(nil), "RequestAuthorize")
 	proto.RegisterType((*ResponseAuthorize)(nil), "ResponseAuthorize")
 	proto.RegisterType((*RequestConnect)(nil), "RequestConnect")
 	proto.RegisterType((*CommandSay)(nil), "CommandSay")
+	proto.RegisterType((*CommandLeave)(nil), "CommandLeave")
 	proto.RegisterType((*Event)(nil), "Event")
 	proto.RegisterType((*EventNone)(nil), "EventNone")
 	proto.RegisterType((*EventJoin)(nil), "EventJoin")
 	proto.RegisterType((*EventLeave)(nil), "EventLeave")
 	proto.RegisterType((*EventLog)(nil), "EventLog")
+	proto.RegisterType((*EventExit)(nil), "EventExit")
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -604,6 +707,7 @@ type ChatClient interface {
 	Authorize(ctx context.Context, in *RequestAuthorize, opts ...grpc.CallOption) (*ResponseAuthorize, error)
 	Connect(ctx context.Context, in *RequestConnect, opts ...grpc.CallOption) (Chat_ConnectClient, error)
 	Say(ctx context.Context, in *CommandSay, opts ...grpc.CallOption) (*None, error)
+	Leave(ctx context.Context, in *CommandLeave, opts ...grpc.CallOption) (*None, error)
 }
 
 type chatClient struct {
@@ -664,11 +768,21 @@ func (c *chatClient) Say(ctx context.Context, in *CommandSay, opts ...grpc.CallO
 	return out, nil
 }
 
+func (c *chatClient) Leave(ctx context.Context, in *CommandLeave, opts ...grpc.CallOption) (*None, error) {
+	out := new(None)
+	err := c.cc.Invoke(ctx, "/Chat/Leave", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // ChatServer is the server API for Chat service.
 type ChatServer interface {
 	Authorize(context.Context, *RequestAuthorize) (*ResponseAuthorize, error)
 	Connect(*RequestConnect, Chat_ConnectServer) error
 	Say(context.Context, *CommandSay) (*None, error)
+	Leave(context.Context, *CommandLeave) (*None, error)
 }
 
 func RegisterChatServer(s *grpc.Server, srv ChatServer) {
@@ -732,6 +846,24 @@ func _Chat_Say_Handler(srv interface{}, ctx context.Context, dec func(interface{
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Chat_Leave_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(CommandLeave)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ChatServer).Leave(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/Chat/Leave",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ChatServer).Leave(ctx, req.(*CommandLeave))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _Chat_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "Chat",
 	HandlerType: (*ChatServer)(nil),
@@ -744,6 +876,10 @@ var _Chat_serviceDesc = grpc.ServiceDesc{
 			MethodName: "Say",
 			Handler:    _Chat_Say_Handler,
 		},
+		{
+			MethodName: "Leave",
+			Handler:    _Chat_Leave_Handler,
+		},
 	},
 	Streams: []grpc.StreamDesc{
 		{
@@ -755,30 +891,33 @@ var _Chat_serviceDesc = grpc.ServiceDesc{
 	Metadata: "proto/chat.proto",
 }
 
-func init() { proto.RegisterFile("proto/chat.proto", fileDescriptor_chat_fe9674db1763fdcb) }
+func init() { proto.RegisterFile("proto/chat.proto", fileDescriptor_chat_3f2ec72249544206) }
 
-var fileDescriptor_chat_fe9674db1763fdcb = []byte{
-	// 340 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x84, 0x92, 0xd1, 0x4e, 0xc2, 0x30,
-	0x14, 0x86, 0x99, 0x6c, 0xe0, 0xce, 0x8c, 0x42, 0xaf, 0x26, 0x09, 0x91, 0x54, 0x63, 0xbc, 0x2a,
-	0x66, 0xde, 0x78, 0xab, 0x84, 0x04, 0x8d, 0xf1, 0xa2, 0x3e, 0x80, 0xa9, 0x70, 0x02, 0x33, 0xac,
-	0x45, 0x5a, 0x48, 0xd4, 0x17, 0xf1, 0x71, 0x4d, 0xbb, 0xc1, 0xa2, 0xce, 0x70, 0xd7, 0xfe, 0xe7,
-	0x6b, 0x4f, 0xff, 0xfe, 0x07, 0x5a, 0x8b, 0xa5, 0x32, 0xaa, 0x3f, 0x9e, 0x09, 0xc3, 0xdc, 0x92,
-	0x36, 0xc0, 0x7f, 0x54, 0x12, 0xe9, 0x39, 0xb4, 0x38, 0xbe, 0xad, 0x50, 0x9b, 0x9b, 0x95, 0x99,
-	0xa9, 0x65, 0xfa, 0x81, 0x84, 0x80, 0x2f, 0x45, 0x86, 0xb1, 0xd7, 0xf3, 0x2e, 0x42, 0xee, 0xd6,
-	0x34, 0x81, 0x36, 0x47, 0xbd, 0x50, 0x52, 0x63, 0x09, 0x76, 0x01, 0x34, 0x6a, 0x9d, 0x2a, 0xf9,
-	0x9c, 0x4e, 0x1c, 0x7e, 0xc0, 0xc3, 0x42, 0xb9, 0x9b, 0xd0, 0x3e, 0x1c, 0x16, 0x77, 0x0f, 0x94,
-	0x94, 0x38, 0x36, 0xbb, 0x0e, 0x0c, 0x01, 0x06, 0x2a, 0xcb, 0x84, 0x9c, 0x3c, 0x89, 0xf7, 0x1d,
-	0x30, 0x89, 0xa1, 0x99, 0xa1, 0xd6, 0x62, 0x8a, 0xf1, 0x9e, 0x7b, 0xe8, 0x66, 0x4b, 0xbf, 0x3c,
-	0x08, 0x86, 0x6b, 0x94, 0x86, 0xf4, 0xc0, 0x97, 0x4a, 0xe6, 0x4e, 0xa2, 0x04, 0x98, 0x53, 0xad,
-	0xef, 0x51, 0x8d, 0xbb, 0x8a, 0x25, 0x5e, 0x55, 0x2a, 0xdd, 0x15, 0x5b, 0xe2, 0x5e, 0xa5, 0xd2,
-	0x12, 0xb6, 0x42, 0x4e, 0x21, 0x98, 0xa3, 0x58, 0x63, 0x5c, 0x77, 0x48, 0x94, 0x23, 0x0f, 0x56,
-	0x1a, 0xd5, 0x78, 0x5e, 0x23, 0x5d, 0xa8, 0xcf, 0xd5, 0x34, 0xf6, 0x1d, 0x12, 0x16, 0x88, 0x9a,
-	0x8e, 0x6a, 0xdc, 0xea, 0xb7, 0x4d, 0x08, 0xd0, 0x4a, 0x34, 0x82, 0x70, 0xfb, 0x06, 0x7a, 0x52,
-	0x6c, 0x6c, 0xbb, 0xca, 0x4f, 0xef, 0x01, 0x94, 0xcd, 0x2a, 0x89, 0x6b, 0xd8, 0xdf, 0xf4, 0xaa,
-	0xaa, 0xff, 0xff, 0x49, 0xc9, 0x27, 0xf8, 0x83, 0x99, 0x30, 0x24, 0x81, 0xb0, 0x0c, 0xb4, 0xcd,
-	0x7e, 0x0f, 0x43, 0x87, 0xb0, 0xbf, 0xb9, 0x9f, 0x41, 0x73, 0x93, 0xe8, 0x11, 0xfb, 0x19, 0x71,
-	0xa7, 0x91, 0x9b, 0xbf, 0xf4, 0xc8, 0x31, 0xd4, 0x6d, 0x8c, 0x11, 0x2b, 0x33, 0xed, 0x04, 0xcc,
-	0x3a, 0x7f, 0x69, 0xb8, 0x21, 0xbc, 0xfa, 0x0e, 0x00, 0x00, 0xff, 0xff, 0xe4, 0x6c, 0x6a, 0x79,
-	0x98, 0x02, 0x00, 0x00,
+var fileDescriptor_chat_3f2ec72249544206 = []byte{
+	// 385 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x84, 0x93, 0xc1, 0x4e, 0xea, 0x40,
+	0x14, 0x86, 0xe9, 0xa5, 0x85, 0xdb, 0x53, 0xee, 0xbd, 0x30, 0xab, 0x5e, 0x12, 0x22, 0xa9, 0xc6,
+	0xb8, 0x71, 0x30, 0x75, 0xe3, 0x56, 0x09, 0x09, 0x1a, 0xe3, 0xa2, 0x3e, 0x80, 0xa9, 0x70, 0x02,
+	0x35, 0x74, 0x06, 0x99, 0x81, 0xa0, 0x8f, 0xe2, 0xa3, 0xf8, 0x74, 0xe6, 0x4c, 0x5b, 0x8a, 0x4a,
+	0x64, 0x37, 0x73, 0xfe, 0xaf, 0x73, 0xfe, 0x39, 0xfd, 0x07, 0x9a, 0xf3, 0x85, 0xd4, 0xb2, 0x37,
+	0x9a, 0xc6, 0x9a, 0x9b, 0x65, 0x50, 0x03, 0xfb, 0x4e, 0x0a, 0x0c, 0x8e, 0xa1, 0x19, 0xe1, 0xf3,
+	0x12, 0x95, 0xbe, 0x5c, 0xea, 0xa9, 0x5c, 0x24, 0xaf, 0xc8, 0x18, 0xd8, 0x22, 0x4e, 0xd1, 0xb7,
+	0xba, 0xd6, 0x89, 0x1b, 0x99, 0x75, 0x10, 0x42, 0x2b, 0x42, 0x35, 0x97, 0x42, 0x61, 0x09, 0x76,
+	0x00, 0x14, 0x2a, 0x95, 0x48, 0xf1, 0x90, 0x8c, 0x0d, 0xde, 0x88, 0xdc, 0xbc, 0x72, 0x3d, 0x0e,
+	0x7a, 0xf0, 0x37, 0x3f, 0xbb, 0x2f, 0x85, 0xc0, 0x91, 0xde, 0xf7, 0xc1, 0x00, 0xa0, 0x2f, 0xd3,
+	0x34, 0x16, 0xe3, 0xfb, 0xf8, 0x65, 0x0f, 0xcc, 0x7c, 0xa8, 0xa7, 0xa8, 0x54, 0x3c, 0x41, 0xff,
+	0x97, 0x31, 0x5a, 0x6c, 0x83, 0x53, 0x68, 0xe4, 0xc7, 0xdc, 0x62, 0xbc, 0xda, 0x6b, 0xf3, 0xdd,
+	0x02, 0x67, 0xb0, 0x42, 0xa1, 0x59, 0x17, 0x6c, 0x21, 0x45, 0x76, 0x71, 0x2f, 0x04, 0x6e, 0xaa,
+	0x34, 0xa6, 0x61, 0x25, 0x32, 0x0a, 0x11, 0x4f, 0x32, 0x11, 0xa6, 0xe3, 0x86, 0xb8, 0x91, 0x89,
+	0x20, 0x82, 0x14, 0x76, 0x08, 0xce, 0x8c, 0xba, 0xfa, 0x55, 0x83, 0x78, 0x19, 0x62, 0x8c, 0x0c,
+	0x2b, 0x51, 0xa6, 0xb1, 0x0e, 0x54, 0x67, 0x72, 0xe2, 0xdb, 0x06, 0x71, 0x73, 0x44, 0x4e, 0x86,
+	0x95, 0x88, 0xea, 0xd4, 0x05, 0xd7, 0x89, 0xf6, 0x9d, 0xed, 0x2e, 0x83, 0x75, 0xa2, 0xa9, 0x0b,
+	0x29, 0x57, 0x75, 0x70, 0x90, 0x8a, 0x81, 0x07, 0xee, 0xc6, 0x65, 0x70, 0x90, 0x6f, 0xc8, 0xd0,
+	0xce, 0xbf, 0xd8, 0x05, 0x28, 0xed, 0xec, 0x24, 0x2e, 0xe0, 0x77, 0xe1, 0x66, 0x97, 0xfe, 0xc3,
+	0xd4, 0x0b, 0x27, 0xe4, 0x33, 0x7c, 0xb3, 0xc0, 0xee, 0x4f, 0x63, 0xcd, 0x42, 0x70, 0xcb, 0xbc,
+	0xb4, 0xf8, 0xd7, 0xac, 0xb5, 0x19, 0xff, 0x1e, 0xab, 0x23, 0xa8, 0x17, 0x81, 0xf9, 0xc7, 0x3f,
+	0x27, 0xa8, 0x5d, 0xcb, 0x86, 0x71, 0x66, 0xb1, 0xff, 0x50, 0xa5, 0x94, 0x78, 0xbc, 0x8c, 0x4c,
+	0xdb, 0xe1, 0x34, 0x07, 0xd6, 0x01, 0x27, 0xbb, 0xe1, 0x1f, 0xbe, 0x1d, 0x84, 0x5c, 0x7e, 0xac,
+	0x99, 0x27, 0x70, 0xfe, 0x11, 0x00, 0x00, 0xff, 0xff, 0xff, 0x49, 0xc9, 0xee, 0x16, 0x03, 0x00,
+	0x00,
 }
